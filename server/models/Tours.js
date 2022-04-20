01b32fea26fa55f925c16ea794f6ed2b9ee1aa01
Brad Kuhne <bjkuhne@aol.com>
2022-04-19 19:39:06 -0500

    updated models: index.js, Tours, and User

---
+++ server/models/Tours.js
@@ -0,0 +1,35 @@
+const { Schema } = require('mongoose');
+
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedtours` array in User.js
+const tourSchema = new Schema({
+  guides: [
+    {
+      type: String,
+    },
+  ],
+  description: {
+    type: String,
+    required: true,
+  },
+  // saved tour id from database
+  tourId: {
+    type: String,
+    required: true,
+  },
+  tourDestination: {
+      type: String,
+      required: true,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  tourName: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = tourSchema;
