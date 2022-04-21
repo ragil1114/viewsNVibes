1404b575f59f5159d417dbcc21f0fd76263a9b9a
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-21 14:20:37 -0500

    fixed spelling mistake

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
         <Navbar bg="light" expand="lg">
   <Container fluid>
-    <Navbar.Brand href="#">viewNVibes</Navbar.Brand>
+    <Navbar.Brand href="#">viewNvibes</Navbar.Brand>
     <Navbar.Toggle aria-controls="navbarScroll" />
     <Navbar.Collapse id="navbarScroll">
       <Nav
@@ -17,8 +17,8 @@ const NavBar = () => {
         <Nav.Link href="#action1">Home</Nav.Link>
         <Nav.Link href="#action2">About</Nav.Link>
         <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Sign In</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Sighn Up</NavDropdown.Item>
+          <NavDropdown.Item href="#action3">Login</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Sign Up</NavDropdown.Item>
           <NavDropdown.Divider />
           <NavDropdown.Item href="#action5">
             Something else here
