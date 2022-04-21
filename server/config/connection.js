b93142d3e2330d1e856276b728a638563c8c6668
Ragil14 <rahil.gilani@gmail.com>
2022-04-21 12:29:41 -0500

    clean code

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/viesnvibes', {  //port changed.  need to verify this works.
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/viewsnvibes', {  //port changed.  need to verify this works.
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
