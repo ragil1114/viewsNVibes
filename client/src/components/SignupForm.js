a12319a3b3ebc962874a4287e55f970bb3a74aeb
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-21 12:49:15 -0500

    imports for signup js

---
+++ client/src/components/SignupForm.js
@@ -0,0 +1,7 @@
+// IMPORTS
+
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import Auth from "../utils/auth";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
