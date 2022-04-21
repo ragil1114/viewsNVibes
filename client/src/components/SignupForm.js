cfc16acd54412c83352dcac59458017a00675867
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-21 12:51:59 -0500

    show alert for server error

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -56,3 +56,17 @@ const SignupForm = () => {
       password: "",
     });
   };
+
+  return (
+    <>
+      {/* This is needed for the validation functionality above */}
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        {/* show alert if server response is bad */}
+        <Alert
+          dismissible
+          onClose={() => setShowAlert(false)}
+          show={showAlert}
+          variant="danger"
+        >
+          Something went wrong with your signup!
+        </Alert>
