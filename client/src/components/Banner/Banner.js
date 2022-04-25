b1e376549016278a4127adb12612cdb82775b975
Mobsiye <ahmobsiye@gmail.com>
2022-04-25 16:52:48 -0500

    photo links

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import '../../../node_modules/materialize-css/dist/css/materialize.min.css';
-import Imgone from '../../images/banner9.jpg';
-import Imgtwo from '../../images/banner6.jpg';
-import Imgthree from '../../images/banner7.jpg';
+import Imgone from '../../images/header1.jpg';
+import Imgtwo from '../../images/header2.jpg';
+import Imgthree from '../../images/header9.jpg';
+import Imgfour from '../../images/header10.jpg';
+import Imgfive from '../../images/header4.jpg';
 import './Banner.css';
 import { Link } from 'react-router-dom';
 
@@ -29,6 +31,22 @@ class Banner extends Component {
                     </li>
                     <li>
                         <img src={Imgthree} alt="Getaways" />
+                        <div className="caption right-align">
+                            <h2>Relax And Enjoy</h2>
+                            <h5 className="light grey-text text-lighten-3 hide-on-small-only">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam non illo earum cumque id est!</h5>
+                            <Link to="#" className="btn btn-large purple">Learn More</Link>
+                        </div>
+                    </li>
+                    <li>
+                        <img src={Imgfour} alt="Beach" />
+                        <div className="caption right-align">
+                            <h2>Relax And Enjoy</h2>
+                            <h5 className="light grey-text text-lighten-3 hide-on-small-only">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam non illo earum cumque id est!</h5>
+                            <Link to="#" className="btn btn-large purple">Learn More</Link>
+                        </div>
+                    </li>
+                    <li>
+                        <img src={Imgfive} alt="Tropical" />
                         <div className="caption right-align">
                             <h2>Relax And Enjoy</h2>
                             <h5 className="light grey-text text-lighten-3 hide-on-small-only">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam non illo earum cumque id est!</h5>
