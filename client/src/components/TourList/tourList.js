4d41a8f74ce7ac753b6f145ab67c28a4bf41b450
ElmerPC <erey2790@gmail.com>
2022-04-27 02:26:58 -0500

    we initialize the instance

--- client/src/components/TourList/tourList.js
+++ client/src/components/TourList/tourList.js
@@ -3,3 +3,8 @@ import {
     InMemoryCache,
     ApolloProvider
   } from "@apollo/client";
+
+  const client = new ApolloClient({
+    uri: 'https://beta.pokeapi.co/graphql/v1beta',
+    cache: new InMemoryCache()
+  });
