b971ee575922960165d34d4fdac824e906bacb9e
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-26 20:01:34 -0500

    copied over files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import NavBar from './components/Navbar';
+import React, { Fragment } from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Banner from './components/Banner/Banner';
+import Footer from './components/Footer/Footer';
+import Search from './components/Search/Search';
+import Explore from './components/Explore/Explore';
+import Adventure from './components/Adventure/Adventure';
+import Booking from './components/Booking/Booking';
+import Gallery from './components/Gallery/Gallery';
+// import Signin from './components/Signin/Signin';
+// import Signup from './components/Signup/Signup'
+import Contact from './components/Contact/Contact';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-    return (
-        <NavBar></NavBar>
-    )
+  return (
+    <Router>
+      <Fragment>
+      <Navbar />
+      <Banner />
+      <Search />
+      <Explore />
+      <Adventure />
+      <Booking />
+      <Gallery />
+      {/* <Signin /> */}
+      {/* <Signup /> */}
+      <Contact />
+      <Footer />
+      </Fragment>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/Search/Search.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../../../../node_modules/materialize-css/dist/css/materialize.min.css';
+import './Search.css';
+
+class Search extends Component {
+  render() {
+    return (
+      <section id="search" className="section section-search blue accent-2 white-text center scrollspy">
+        <div className="container">
+          <div className="row">
+            <div className="col s12">
+              <div className="input-field">
+                <input className="white grey-text autocomplete" placeholder="Search for places, hotels, ect..." type="text" id="autocomplete-input" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Search;
