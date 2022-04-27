e898aafa8d955a6ccc03e73c11cd57bd759226bc
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-26 21:12:21 -0500

    fixed contact section

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -26,7 +26,7 @@ class Contact extends Component {
                         </div>
                         <div className="col s12 m6">
                             <div className="card-panel grey lighten-3">
-                                <h5>Please fill out this form</h5>
+                                <h5>Contact Us!</h5>
                                 <form>
                                     <div className="input-field">
                                         <input type="text" id="name" className="validate" />
