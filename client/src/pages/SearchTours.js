b615385591e3d2a83b6df390a4fd832cdbf002d7
Brad Kuhne <bjkuhne@aol.com>
2022-04-28 09:16:43 -0500

    updated SearchTour other than query call

--- client/src/pages/SearchTours.js
+++ client/src/pages/SearchTours.js
@@ -1,1 +1,152 @@
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
+import Auth from '../utils/auth';
+import { saveTourIds, getSavedTourIds } from '../utils/localStorage';
+
+//added
+import { useMutation } from '@apollo/client';
+import { SAVE_TOUR } from '../utils/mutations';
+// import { saveTour } from '../../../server/controllers/user-controller';
+
+const SearchTours = () => {
+  // create state for holding returned tour data from query
+  const [searchedTours, setSearchedTours] = useState([]);
+  // create state for holding our search field data
+  const [searchInput, setSearchInput] = useState('');
+
+  // create state to hold saved tourId values
+  const [savedTourIds, setSavedTourIds] = useState(getSavedTourIds());
+
+  // set up useEffect hook to save `savedTourIds` list to localStorage on component unmount
+  useEffect(() => {
+    return () => saveTourIds(savedTourIds);
+  });
+
+  const [saveTour, { error }] = useMutation(SAVE_TOUR);
+
+  // create method to search for tourss and set state on form submit
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!searchInput) {
+      return false;
+    }
+// THIS CODE NEEDS TO BE UPDATED TO CALL GET_TOURS QUERY 
+    try {
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`);
+
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+
+      const { items } = await response.json();
+
+      const tourData = items.map((tour) => ({
+        tourId: tour.tourId,
+        tourGuide: tour.tourGuide,
+        tourDestination: tour.tourDestination,
+        tourName: tour.tourName,
+        description: tour.description,
+        image: tour.image,
+      }));
+
+      setSearchedTour(tourData);
+      setSearchInput('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // create function to handle saving a tour to our database
+  const handleSaveTour = async (tourId) => {
+    // find the tour in `searchedTours` state by the matching id
+    const tourToSave = searchedTours.find((tour) => tour.tourId === tourId);
+
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const updatedUser = await saveTour({ variables: { ...tourToSave } });
+
+      if (!updatedUser) {
+        throw new Error('something went wrong!');
+      }
+
+      // if tour successfully saves to user's account, save tour id to state
+      setSavedTourIds([...savedTourIds, tourToSave.tourId]);
+      saveTourIds(saveTourIds);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <Jumbotron fluid className='text-light bg-dark'>
+        <Container>
+          <h1>Search for Tours!</h1>
+          <Form onSubmit={handleFormSubmit}>
+            <Form.Row>
+              <Col xs={12} md={8}>
+                <Form.Control
+                  name='searchInput'
+                  value={searchInput}
+                  onChange={(e) => setSearchInput(e.target.value)}
+                  type='text'
+                  size='lg'
+                  placeholder='Search for a tour'
+                />
+              </Col>
+              <Col xs={12} md={4}>
+                <Button type='submit' variant='success' size='lg'>
+                  Submit Search
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Container>
+      </Jumbotron>
+
+      <Container>
+        <h2>
+          {searchedTours.length
+            ? `Viewing ${searchedTours.length} results:`
+            : 'Search for a tour to begin'}
+        </h2>
+        <CardColumns>
+          {searchedTours.map((tour) => {
+            return (
+              <Card key={tour.tourId} border='dark'>
+                {tour.image ? (
+                  <Card.Img src={tour.image} alt={`${tour.tourName}`} variant='top' />
+                ) : null}
+                <Card.Body>
+                  <Card.Title>{tour.tourName}</Card.Title>
+                  <p className='small'>Tour Guide: {tour.tourGuide}</p>
+                  <Card.Text>{tour.description}</Card.Text>
+                  {Auth.loggedIn() && (
+                    <Button
+                      disabled={savedTourIds?.some((savedTourId) => savedTourId === tour.tourId)}
+                      className='btn-block btn-info'
+                      onClick={() => handleSaveTour(tour.TourId)}>
+                      {savedTourIds?.some((savedTourId) => savedTourId === tour.tourId)
+                        ? 'This tour has already been saved!'
+                        : 'Save this Tour!'}
+                    </Button>
+                  )}
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
+};
+
+export default SearchTours;
