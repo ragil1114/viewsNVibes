4406e8c967b31913e865276f80992420616db383
ElmerPC <erey2790@gmail.com>
2022-04-27 22:47:59 -0500

    imported my files to searchTours

--- client/src/pages/SearchTours.js
+++ client/src/pages/SearchTours.js
@@ -1,1 +1,8 @@
+import React, { useState, useEffect } from 'react';
+// import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
+// import Auth from '../utils/auth';
+
+//added
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
