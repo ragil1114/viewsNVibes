325d1dacaf69c27c4271871c7d19b3a96d7dd211
ElmerPC <erey2790@gmail.com>
2022-04-27 23:03:05 -0500

    created my state variables

--- client/src/pages/SearchTours.js
+++ client/src/pages/SearchTours.js
@@ -5,4 +5,18 @@ import React, { useState, useEffect } from 'react';
 
 //added
 import { useMutation } from '@apollo/client';
-import { SAVE_BOOK } from '../utils/mutations';
+import { SAVE_TOUR } from '../utils/mutations';
+
+const SearchTours = () => {
+ // CREATE STATE FOR HOLDING RETURNED GRAPHQL QUERY
+  const [searchedTours, setSearchBooks] = useState([]);
+
+  // CREATE STATE FOR HOLDING OUR SEARCH FIELD DATA
+  const [searchInput, setsearchInput] = useState('');
+
+  // CREATE STATE TO HOLD SAVED BOOKID VALUES
+  const [savedTourIds, setSavedTourIds] = useState(getSavedTourIds());
+}
+
+
+export default SearchTours
